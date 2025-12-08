3fcff2204d407b15a2a36764c7b355df44e0b4c2
Sri Mounika <keerthisrimounika@gmail.com>
2025-12-08 19:39:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fill LinkedList/fill-linked-list.java
@@ -0,0 +1,30 @@
+// User function Template for Java
+class Solution {
+    public static void fillLinkedList(LinkedList<Integer> ll, int k) {
+        // Your code here
+        for(int i=0; i<ll.size(); i++)
+        {
+           
+                 ll.set(i,k);
+        }
+        // for(int val : ll)
+        // {
+        //          System.out.print(val + " ");
+       
+            
+        // }   
+              
+       
+             
+      
+            
+            
+        
+            
+           
+        
+       
+       
+        
+    }
+}
