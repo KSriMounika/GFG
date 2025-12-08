7915e9fd8f1fff3ba43a2fa862deec39e2513120
Sri Mounika <keerthisrimounika@gmail.com>
2025-12-08 14:13:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LinkedList Insertion/linked-list-insertion.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+class Solution {
+    public static LinkedList<Integer> insertion(int arr[]) {
+        // Your code here
+        LinkedList <Integer> list = new LinkedList<>();
+        for(int n: arr)
+        {
+            list.add(n);
+        }
+        return list;
+    }
+}
