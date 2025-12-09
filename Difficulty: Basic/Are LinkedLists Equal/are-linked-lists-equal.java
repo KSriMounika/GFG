998461b6f7bed4a01d135cdcf8bfd00416b2caa6
Sri Mounika <keerthisrimounika@gmail.com>
2025-12-09 14:13:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Are LinkedLists Equal/are-linked-lists-equal.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    public static boolean areEqual(LinkedList<Integer> ll1, LinkedList<Integer> ll2) {
+        // Your code here
+        if(ll1.equals(ll2)) return true;
+        else return false;
+    }
+}
